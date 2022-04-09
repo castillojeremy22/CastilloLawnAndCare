54859d50ee9c3ad83aa117ff70152fd16ba237ce
Jeremy Castillo <castillojeremy22@gmail.com>
2022-04-08 21:50:13 -0500

    pushing view model for customer review

---
+++ CastilloLawnCare/Models/ViewModels/vmCustomerReview.cs
@@ -0,0 +1,22 @@
+﻿namespace CastilloLawnCare.Models
+{
+    namespace ViewModels
+    {
+        public class vmCustomerReview : User
+        {
+            public int ReviewID { get; set; }
+            public string Description { get; set; }
+
+            public vmCustomerReview()
+            {
+            }
+
+            public vmCustomerReview(int reviewID, string description)
+            {
+                ReviewID = reviewID;
+                Description = description;
+            }
+        }
+    }
+    
+}
